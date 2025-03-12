027f87019c0b4dffa383fca704a23874ec1bc3b7
Bishal Adhikari <6680800+adhikari-bishal@users.noreply.github.com>
2025-03-12 15:17:38 +0545

    cleanup build artifacts

--- src/types.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'react-native-vector-icons/MaterialCommunityIcons' {
-    import { IconProps } from 'react-native-vector-icons/Icon';
-    import { Component } from 'react';
-    export default class MaterialCommunityIcons extends Component<IconProps> {
-    }
-}
