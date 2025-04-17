cbde595287fe5d3d234901347db4180630aaeb3c
Bishal Adhikari <6680800+adhikari-bishal@users.noreply.github.com>
2025-04-17 17:45:22 +0545

    fixes #5 : refactor textinput component

--- src/components/forms/TextInput.tsx
+++ src/components/forms/TextInput.tsx
@@ -1,31 +1,33 @@
 import {Controller} from "react-hook-form";
 import React from "react";
 import PropTypes from "prop-types";
-import {StyleSheet, Text, TextInput as Input, View} from "react-native";
+import {StyleSheet, Text, TextInput as Input, View, ViewStyle,} from "react-native";
 import colors from "../../constants/colors";
 
 interface TextInputProps {
     control: any;
     name: string;
-    label: string;
-    placeHolder?: string;
-    successMessage?: string;
+    label?: string;
+    placeholder?: string;
     validationRules?: object;
     errorMessage?: string;
-    successColor?: string;
     errorColor?: string;
+    style?: ViewStyle;
+    leftContent?: React.ReactNode;
+    rightContent?: React.ReactNode;
 }
 
 const TextInput: React.FC<TextInputProps> = ({
                                                  control,
                                                  name,
                                                  label,
-                                                 placeHolder,
-                                                 successMessage,
+                                                 placeholder,
                                                  validationRules,
                                                  errorMessage,
-                                                 successColor = colors.success,
                                                  errorColor = colors.danger,
+                                                 style,
+                                                 leftContent,
+                                                 rightContent,
                                              }) => {
     return (
         <Controller
@@ -36,39 +38,46 @@ const TextInput: React.FC<TextInputProps> = ({
                          field: {onChange, onBlur, value},
                          fieldState: {error},
                      }) => (
-                <View style={styles.container}>
-                    <Text
-                        style={{
-                            color: error
-                                ? error.type === "success"
-                                    ? successColor
-                                    : errorColor
-                                : colors.black,
-                        }}
-                    >
-                        {label}
-                    </Text>
-
-                    <Input
-                        value={value}
-                        onChangeText={onChange}
-                        onBlur={onBlur}
+                <View style={[styles.container]}>
+                    {label && (
+                        <Text
+                            style={{
+                                color: error ? errorColor : colors.black,
+                                marginBottom: 4,
+                            }}
+                        >
+                            {label}
+                        </Text>)}
+                    <View
                         style={[
-                            styles.input,
+                            styles.inputContainer, style,
                             {borderColor: error ? errorColor : colors.black},
                         ]}
-                        placeholder={placeHolder}
-                    />
+                    >
+                        {leftContent && (
+                            <View style={styles.sideContent}>{leftContent}</View>
+                        )}
+
+                        <Input
+                            value={value}
+                            onChangeText={onChange}
+                            onBlur={onBlur}
+                            style={styles.input}
+                            placeholder={placeholder}
+                            accessibilityLabel={label || placeholder}
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            keyboardType="default"
+                        />
+
+                        {rightContent && (
+                            <View style={styles.sideContent}>{rightContent}</View>
+                        )}
+                    </View>
 
                     {error && (
                         <Text style={[styles.message, {color: errorColor}]}>
                             {error.message || errorMessage || "Invalid input"}
-                        </Text>
-                    )}
-
-                    {!error && successMessage && (
-                        <Text style={[styles.message, {color: successColor}]}>
-                            {successMessage}
                         </Text>
                     )}
                 </View>
@@ -79,34 +88,43 @@ const TextInput: React.FC<TextInputProps> = ({
 
 const styles = StyleSheet.create({
     container: {
-        marginVertical: 5,
+        width: "100%",
+        marginVertical: 6,
     },
-    input: {
-        color: colors.black,
-        fontSize: 16,
+    inputContainer: {
+        flexDirection: "row",
+        alignItems: "center",
         borderWidth: 1,
         borderRadius: 8,
-        marginVertical: 6,
-        paddingHorizontal: 8,
-        paddingVertical: 0,
-        minHeight: 44,
+        paddingHorizontal: 4,
+        height: 44,
+    },
+    input: {
+        flex: 1,
+        color: colors.black,
+        fontSize: 16,
+        paddingVertical: 8,
+    },
+    sideContent: {
+        marginHorizontal: 4,
     },
     message: {
         fontSize: 13,
-        alignSelf: "stretch",
+        marginTop: 4,
     },
 });
 
 TextInput.propTypes = {
     control: PropTypes.object.isRequired,
     name: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
-    placeHolder: PropTypes.string,
-    successMessage: PropTypes.string,
+    label: PropTypes.string,
+    placeholder: PropTypes.string,
     validationRules: PropTypes.object,
     errorMessage: PropTypes.string,
-    successColor: PropTypes.string,
     errorColor: PropTypes.string,
+    style: PropTypes.object,
+    leftContent: PropTypes.element,
+    rightContent: PropTypes.element,
 };
 
 export default TextInput;
