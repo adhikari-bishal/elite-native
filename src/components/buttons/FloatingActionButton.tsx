5b2396735d19a11b8acf7c71f013a6b0d9e37fee
Bishal Adhikari <6680800+adhikari-bishal@users.noreply.github.com>
2025-04-20 22:42:39 +0545

    fixes #4 : add customization options to floating action button

--- src/components/buttons/FloatingActionButton.tsx
+++ src/components/buttons/FloatingActionButton.tsx
@@ -1,47 +1,65 @@
 import React from "react";
-import {StyleSheet, TouchableOpacity, TouchableOpacityProps, ViewStyle,} from "react-native";
-import Icons from "react-native-vector-icons/MaterialCommunityIcons";
+import {StyleProp, StyleSheet, TouchableOpacity, TouchableOpacityProps, ViewStyle,} from "react-native";
 import colors from "../../constants/colors";
 
+type Position = "left" | "center" | "right";
+
 interface FloatingActionButtonProps extends TouchableOpacityProps {
-    icon: string;
+    icon: React.ReactNode;
     onPress: () => void;
     fabColor?: string;
-    iconColor?: string;
+    position?: Position;
+    containerStyle?: StyleProp<ViewStyle>;
 }
 
 const FloatingActionButton: React.FC<FloatingActionButtonProps> = ({
                                                                        icon,
                                                                        onPress,
                                                                        fabColor = colors.primary,
-                                                                       iconColor = colors.white,
+                                                                       position = "right",
+                                                                       containerStyle,
                                                                        ...props
                                                                    }) => {
-    const containerStyle = {backgroundColor: fabColor, ...style.container};
+    const positionStyle: ViewStyle = {
+        position: "absolute",
+        bottom: 16,
+        ...(position === "left"
+            ? {left: 16}
+            : position === "center"
+                ? {left: "50%", transform: [{translateX: -28}]}
+                : {right: 16}),
+    };
 
     return (
         <TouchableOpacity
             testID="fab-container"
             onPress={onPress}
-            style={containerStyle}
+            style={[
+                styles.fab,
+                positionStyle,
+                {backgroundColor: fabColor},
+                containerStyle,
+            ]}
             {...props}
         >
-            <Icons testID="fab-icon" name={icon} size={24} color={iconColor}/>
+            {icon}
         </TouchableOpacity>
     );
 };
 
-const style = StyleSheet.create({
-    container: {
-        position: "absolute",
-        bottom: 16,
-        right: 16,
+const styles = StyleSheet.create({
+    fab: {
         width: 56,
         height: 56,
         borderRadius: 28,
         justifyContent: "center",
         alignItems: "center",
-    } as ViewStyle,
+        elevation: 5,
+        shadowColor: "#000",
+        shadowOpacity: 0.2,
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 4,
+    },
 });
 
 export default FloatingActionButton;
